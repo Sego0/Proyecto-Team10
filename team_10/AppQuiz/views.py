41708bb5f9d9b4d5e6ef92160f5370a7abb84dd2
Bastián Solar V <bastian.solar@sansano.usm.cl>
2021-12-04 12:24:17 -0300

    Funcionamiento

--- team_10/AppQuiz/views.py
+++ team_10/AppQuiz/views.py
@@ -1,12 +1,19 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def inicio(request):
     return render(request, "portada.html")
 
 def _1er_escenario(request):
-    return render(request, "Pieza.html")
-
+    objetos = request.GET.get('objects', '0000000')
+    if objetos == "1111":
+        redirect("123")
+    objetos_l = list(objetos)
+    context = {
+        "objetos" : objetos_l,
+        "objetosAcualtes": objetos,
+    }
+    return render(request, "Pieza.html", context)
 
 def _2do_escenario_1(request):
     return render(request, "Casa.html")
