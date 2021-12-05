bb13010fd5c8fd6f88a294f49421656596d52df9
Bastián Solar V <bastian.solar@sansano.usm.cl>
2021-12-05 12:30:12 -0300

    Explosion

--- team_10/AppQuiz/views.py
+++ team_10/AppQuiz/views.py
@@ -5,9 +5,9 @@ def inicio(request):
     return render(request, "portada.html")
 
 def _1er_escenario(request):
-    objetos = request.GET.get('objects', '0000000')
-    if objetos == "1111":
-        redirect("123")
+
+    objetos = request.GET.get('objects', '0000')
+
     objetos_l = list(objetos)
     context = {
         "objetos" : objetos_l,
@@ -15,11 +15,20 @@ def _1er_escenario(request):
     }
     return render(request, "Pieza.html", context)
 
-def _2do_escenario_1(request):
-    return render(request, "Casa.html")
 
+def _2do_escenario_1(request):
+
+    objectsPrev = request.GET.get('objectsPrev', '0000')
+    if "0" in objectsPrev:
+        return redirect("/AppQuiz/explosion/")
+
+    return render(request, "Casa.html")
+ 
 def _2do_escenario_2(request):
     return render(request, "Casa.html")    
 
 def _2do_escenario_3(request):
     return render(request, "Casa.html")    
+
+def explosion(request):
+    return render(request, "explosion.html")
