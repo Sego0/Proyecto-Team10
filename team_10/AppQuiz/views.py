83a2adf1198275d597646778305e75f303d39536
Diecma <diego.cortes-monroy@usm.cl>
2021-12-07 20:56:52 -0300

    TODO LISTO HASTA LIVING

--- team_10/AppQuiz/views.py
+++ team_10/AppQuiz/views.py
@@ -4,7 +4,9 @@ from .forms import UsuarioLoginFormulario, RegistroFormulario
 from django.shortcuts import render, redirect 
 
 def inicio(request):
+
     return render(request, "portada.html")
+
 
 def _1er_escenario(request):
 
@@ -22,11 +24,11 @@ def _1er_escenario(request):
 
 def _2do_escenario_1(request):
 
-    objectsPrev = request.GET.get('objectPrev', '0000')
+    objectsPrev = request.GET.get('objectsPrev', '0000')
     if "0" in objectsPrev:
         return redirect("/explosion/")
 
-    return render(request, "Casa.html")
+    return render(request, "living.html")
 
 def _2do_escenario_2(request):
     return render(request, "Casa.html")    
@@ -36,43 +38,5 @@ def _2do_escenario_3(request):
 
 def explosion(request):
     return render(request, "explosion.html")
-def loginView(request):
-    titulo = 'login'
-    form = UsuarioLoginFormulario(request.POST or None)
-    if form.is_valid():
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-        usuario = authenticate(username=username, password=password)
-        login(request, usuario)
-        return redirect('inicio')
-
-    context = {
-        'form' : form,
-        'titulo' : titulo
-    }
-
-    return render(request, 'Usuario/login.html', context)
-
-def registro(request):
-
-    titulo = 'Crear una cuenta'
-    if request.method == 'POST':
-        form = RegistroFormulario(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = RegistroFormulario()
-
-    context = {
-
-        'form': form,
-        'titulo': titulo
-
-    }
-
-    return render(request, 'Usuario/registro.html', context)
-
-def logout_vista(request):
-    logout(request)
-    return redirect('/')
+def parque(request):
+    return render(request, "parque.html")
