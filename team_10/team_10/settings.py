ed8361d4a7df76f20600f51a471026bd0a553680
Diecma <diego.cortes-monroy@usm.cl>
2021-12-07 18:42:23 -0300

    Static arreglado

--- team_10/team_10/settings.py
+++ team_10/team_10/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = ['diecma.pythonanywhere.com']
+ALLOWED_HOSTS = ['diecma.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
